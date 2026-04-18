748b2e9fb86228ec6e733e7045cd9555a19a9a2c
bashonly <bashonly@protonmail.com>
2026-04-18 09:28:15 -0500

    [ci] improve helper

--- .github/scripts/output_variables.py
+++ .github/scripts/output_variables.py
@@ -8,10 +8,6 @@ import sys
 
 sys.path.insert(0, str(pathlib.Path(__file__).parent.parent.parent))
 
-if not os.getenv('GITHUB_ACTIONS'):
-    print('This script is intended to be run in a GitHub Actions environment', file=sys.stderr)
-    sys.exit(1)
-
 from bot.knowledge import (
     DEFAULT_HEAD_BRANCHES,
     DEFAULT_HEAD_OWNER,
@@ -20,15 +16,19 @@ from bot.knowledge import (
 
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='this script is intended to be run in a GitHub Actions environment')
     parser.add_argument(
         'service',
         choices=['actions'],
+        help='output the variables for this service',
     )
     service = parser.parse_args().service
-
+    in_gha = os.getenv('GITHUB_ACTIONS')
     default_head_branch = DEFAULT_HEAD_BRANCHES[service]
     matrix = [v for v in SERVICED_REPOS.values() if service in v['services']]
+
+    if in_gha:
+        print('::group::Output variables')
 
     print(
         json.dumps(
@@ -40,12 +40,18 @@ def main():
             indent=2,
         ),
     )
+    if in_gha:
+        print('::endgroup::')
+    else:
+        return 1
 
     with open(os.environ['GITHUB_OUTPUT'], 'a') as f:
         f.write(f'default_head_branch={default_head_branch}\n')
         f.write(f'default_head_owner={DEFAULT_HEAD_OWNER}\n')
         f.write(f'matrix={json.dumps(matrix)}')
 
+    return 0
+
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
